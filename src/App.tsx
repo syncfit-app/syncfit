87692c7297300982ecb9410f9c6b654756bde574
syncfit-app <sfitapp.official@gmail.com>
2026-08-26 02:18:36 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ export function App() {
           <Route
             path="/"
             element={
-              <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pb-32">
+              <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pb-8">
                 <div className="lg:col-span-2 space-y-6">
                   <HeroBanner />
                   <MetricsGrid />
