1af1405fcca8fb6312d25dcbb284e69a271990c6
syncfit-app <sfitapp.official@gmail.com>
2026-08-26 02:01:00 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,6 @@ export function App() {
           <Route
             path="/"
             element={
-              {/* Tambahan pb-32 di sini agar tidak tertutup BottomNav */}
               <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 pb-32">
                 <div className="lg:col-span-2 space-y-6">
                   <HeroBanner />
